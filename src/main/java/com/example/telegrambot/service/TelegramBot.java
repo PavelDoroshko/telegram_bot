ebd4b7eda8fe7c16cae4f7e18707dc66a3ce2390
pavel <e-elez@mail.ru>
2022-09-23 17:05:28 +0300

    telegram_bot

---
+++ src/main/java/com/example/telegrambot/service/TelegramBot.java
@@ -0,0 +1,74 @@
+package com.example.telegrambot.service;
+
+import com.example.telegrambot.config.BotConfig;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig botConfig;
+
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/time":
+                    LocalDateTime localDateTime = LocalDateTime.now();
+                    sendMessage(chatId, localDateTime.toString());
+                    break;
+                default:
+                    sendMessage(chatId, "sorry");
+            }
+
+        }
+
+    }
+
+    @SneakyThrows
+    private void startCommandReceived(long id, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!";
+        log.info("Replied to user" + name);
+        sendMessage(id, answer);
+    }
+
+    @SneakyThrows
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("error : " + e.getMessage());
+        }
+    }
+}
